512285d77fd203c0d073507c5eddb7cc99d2bdfa
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-10-13 14:51:08 +0300

    project - task - task status entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TaskStatusModule } from './task-status/task-status.module';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    TaskStatusModule,
+    ProjectModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
